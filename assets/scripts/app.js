baf1c4034d6c226f5960fbe2b6e6a7cd77863b29
Amir <dev.amirhemmati1382@gmail.com>
2020-11-09 17:53:34 +0330

    responsive top-nav, header, section-description, section-product-menu

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,15 @@
-// tooltip config for bs4
+const mobileMenuIcon = document.querySelector("#mobile-menu-icon");
+
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip({ trigger: "hover" });
+});
+
+// # mobile menu icon click handler
+// function clickMobileMenuIconHandler() {
+// 	mobileMenuIcon.classList.toggle("header__nav_mobile-men	-icon-active");
+// }
+
+$(document).ready(function () {
+	// # mobile menu icon run click handler
+	// mobileMenuIcon.addEventListener("click", clickMobileMenuIconHandler);
 });
