d5d91ee5c059f27404f1379883d0255dabc026f9
Amir <dev.amirhemmati1382@gmail.com>
2020-11-13 02:46:33 +0330

    add tab feature to project

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,6 +30,44 @@ function initialize() {
 	}, 200);
 }
 
+const tabHandler = (tabGroupName, activeClass) => {
+	const tabGroupNameItems = document.querySelectorAll(
+		`[data-tab-group-name=${tabGroupName}]`
+	);
+	const tabItems = document.querySelectorAll(
+		`[data-tab-group-name-item=${tabGroupName}]`
+	);
+
+	let activeItemIndex;
+	Array.from(tabGroupNameItems).forEach((tabItem, index) => {
+		const dataTabActiveAttr = tabItem.getAttribute("data-tab-active");
+
+		if (dataTabActiveAttr === "true") {
+			tabItem.classList.add(activeClass);
+			activeItemIndex = index;
+			tabItems[index].classList.add("d-block");
+		} else {
+			tabItems[index].classList.add("d-none");
+		}
+
+		tabItem.addEventListener("click", (event) => {
+			event.preventDefault();
+
+			Array.from(tabGroupNameItems)[activeItemIndex].classList.remove(
+				activeClass
+			);
+
+			Array.from(tabItems)[activeItemIndex].classList.remove("d-block");
+			Array.from(tabItems)[activeItemIndex].classList.add("d-none");
+
+			Array.from(tabItems)[index].classList.add("d-block");
+
+			activeItemIndex = index;
+			tabItem.classList.add(activeClass);
+		});
+	});
+};
+
 // # mobile menu icon click handler
 function clickMobileMenuIconHandler() {
 	mobileMenuIcon.classList.toggle("header__nav_mobile-menu-icon-active");
@@ -39,6 +77,8 @@ function clickMobileMenuIconHandler() {
 
 $(document).ready(function () {
 	initialize();
+
+	tabHandler("popular-product", "popular-products__btn-group_active-btn");
 
 	// # mobile menu icon run click handler
 	mobileMenuIcon.addEventListener("click", clickMobileMenuIconHandler);
